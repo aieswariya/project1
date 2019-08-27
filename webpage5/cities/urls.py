f16f828b12db8987a74142fcd7c213afcd2ef243
aieswariya <routrayaieswariya1996@gmail.com>
2019-08-27 03:47:17 -0700

    urls py normal change

--- webpage5/cities/urls.py
+++ webpage5/cities/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 
 from .views import homepage, searchresult
-
 urlpatterns = [
     path('', homepage.as_view(), name='homepage'),
     path('search', searchresult.as_view(), name='search_result'),
